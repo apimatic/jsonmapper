5f8ccfd9c52259f0a9c7305bb95940e0c597d66d
Muhammad Haris <Agha.Mohammad.Haris@gmail.com>
2022-05-20 14:22:07 +0500

    Debug Github Actions failure.

--- tests/JsonMapperTest.php
+++ tests/JsonMapperTest.php
@@ -411,9 +411,6 @@ class JsonMapperTest extends \PHPUnit\Framework\TestCase
     public function testOpCacheSaveCommentsDiscarded()
     {
         echo ("Starting testOpCacheSaveCommentsDiscarded!" . "\n");
-        print("Starting testOpCacheSaveCommentsDiscarded!" . "\n");
-        fwrite(STDERR, print_r("Starting testOpCacheSaveCommentsDiscarded!" . "\n", TRUE));
-        fwrite(STDOUT, print_r("Starting testOpCacheSaveCommentsDiscarded!" . "\n", TRUE));
 
         $this->expectException(JsonMapperException::class);
         $this->expectExceptionMessage("Comments cannot be discarded in the configuration file i.e. the php.ini file; doc comments are a requirement for JsonMapper. Following configuration keys must have a value set to \"1\": zend_optimizerplus.save_comments, opcache.save_comments.");
@@ -424,22 +421,28 @@ class JsonMapperTest extends \PHPUnit\Framework\TestCase
 
         try
         {
+            echo ("Inside Try!" . "\n");
             new JsonMapperCommentsDiscardedException($config);
+            echo ("After JsonMapper Instantiation!" . "\n");
         }
 
         catch(JsonMapperException $ex)
         {
+            echo ("Inside specific JsonMapperException catch!" . "\n");
             print_r($ex instanceof JsonMapperException . "\n");
             fwrite(STDERR, print_r($ex instanceof JsonMapperException . "\n", TRUE));
+            echo ("Ending specific JsonMapperException catch!" . "\n");
         }
 
         catch (Exception $ex)
         {
+            echo ("Inside generic JsonMapperException catch!" . "\n");
             print_r(gettype($ex) . "\n");
             print_r($ex  . "\n");
             fwrite(STDERR, print_r(gettype($ex) . "\n", TRUE));
             fwrite(STDERR, print_r($ex  . "\n", TRUE));
             fwrite(STDERR, print_r("In generic Exception block!"  . "\n", TRUE));
+            echo ("Ending generic JsonMapperException catch!" . "\n");
         }
     }
 
