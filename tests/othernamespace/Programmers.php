699f0ace4be6a3e947d65cdcb37f968a6bca4d85
Asad Ali <asad.ali@apimatic.io>
2022-12-06 18:47:07 +0500

    fix(multi-namespace-inheritence): fixed declaring class namespace

---
+++ tests/othernamespace/Programmers.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace othernamespace;
+
+use namespacetest\model\Group;
+
+class Programmers extends Group
+{
+    /**
+     * @maps language
+     * @var string
+     */
+    public $language;
+}
